befe5cc5bb071d37b11735a8082dc6f2370b2aa5
Leader153 <danielflider78@gmail.com>
2026-02-21 03:09:46 +0200

    Robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  site: 'https://leader-sailing.co.il', // ← замени на свой домен
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+      },
+    }),
+    sitemap(),
+  ],
 });
