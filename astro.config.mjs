8dc3345f1f48d269ee478e14cd3b4b229ddbbd47
Leader153 <danielflider78@gmail.com>
2026-02-22 21:53:33 +0200

    "config fix for cloudflare"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://derechalhaosher.netlify.app',
+  //site: 'https://raspy-bar-48d5.pages.dev',
   integrations: [
     partytown({
       config: {
-        forward: ['dataLayer.push', 'gtag'],
+        forward: ["dataLayer.push", "gtag"],
       },
     }),
     sitemap(),
